19b6c3e134be3dde0a1fb744ab039e65d734c51d
Tyler Stewart <tylers490@gmail.com>
2019-10-31 17:17:47 -0600

    feat: link to api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Uuid from './Uuid';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
 import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
 import './App.css';
 
@@ -11,6 +12,9 @@ const App: React.FC = () => {
         <Uuid />
       </div>
       <div className="footer">
+        <a href="/api" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faCode} />
+        </a>
         <a href="https://github.com/trs/getuuid" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faGithubAlt} />
         </a>
